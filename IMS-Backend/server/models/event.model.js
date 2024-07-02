c4b9cce561d1a0685c92724eaa8efe1499953825
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-02 17:20:24 -0400

    Create new model for event

--- IMS-Backend/server/models/event.model.js
+++ IMS-Backend/server/models/event.model.js
@@ -1,1 +1,46 @@
 const { Schema, model } = require("mongoose");
+
+const User = require("./user.model");
+
+const eventSchema = new Schema(
+  {
+    user_id: {
+      type: String,
+      ref: "User",
+      required: true
+    },
+    event_name: {
+      type: String,
+      required: true,
+      maxlength: 100
+    },
+    event_place: {
+      type: String,
+      default: ''
+    },
+    event_date: {
+      type: Date,
+      required: true
+    },
+    event_time: {
+      type: String,
+      required: true
+    },
+    event_description: {
+      type: String,
+      required: false,
+      maxlength: 500
+    },
+    event_type: {
+      type: String,
+      required: false
+    },
+    participants: {
+      type: Number,
+      required: false
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Event", eventSchema);
