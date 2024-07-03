f02ade8a7657919e01c5232f951dacc07b12b1f7
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-03 00:11:56 -0400

    Add the Drawer component with the form

--- IMS-Frontend/src/assets/Components/AdminDashboard/EventManagement/index.jsx
+++ IMS-Frontend/src/assets/Components/AdminDashboard/EventManagement/index.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import { Drawer } from "../../Common/Drawer";
 import ApiService from "../../../../Utils/ApiService";
 
 export const EventManagement = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -24,5 +26,53 @@ export const EventManagement = () => {
     getCourses();
   }, []);
 
-  return <div>Event Management</div>;
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Add Event
+      </button>
+
+      <Drawer
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+        title={"Add Event"}
+        footer={
+          <>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+            <button>Submit</button>
+          </>
+        }
+      >
+        <div className="container form-container">
+          <h2 className="form-heading heading">Schedule Event</h2>
+          <hr />
+          <form>
+            <div className="form-group">
+              <label htmlFor="event-name">Event Name:</label>
+              <input type="text" id="event-name" name="event-name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="event-place">Event Place:</label>
+              <input type="text" id="event-place" name="event-place" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="event-date">Event Date:</label>
+              <input type="date" id="event-date" name="event-date" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="event-time">Event Time:</label>
+              <input type="time" id="event-time" name="event-time" required />
+            </div>
+            <button type="submit" className="btn custome-blue-submit">Add Event</button>
+          </form>
+        </div>
+      </Drawer>
+    </>
+  );
 };
