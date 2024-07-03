c1a2e91b48ef8a4932a909513b92e9b394b68aaf
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-03 00:06:46 -0400

    setting up event component

---
+++ IMS-Frontend/src/assets/Components/AdminDashboard/EventManagement/index.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import ApiService from "../../../../Utils/ApiService";
+
+export const EventManagement = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCourses = async () => {
+    try {
+      setLoading(true);
+      const resp = await ApiService.get(ApiService.ApiURLs.getCourses);
+      if (resp.status === 200 && resp.data?.data) {
+        console.log(resp.data.data);
+        setCourses(resp.data.data);
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
+  return <div>Event Management</div>;
+};
