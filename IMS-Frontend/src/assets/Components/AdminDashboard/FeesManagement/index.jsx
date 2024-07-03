7b5b861530bcac470cdb6c34dfca712ef2e7605d
“msnavjivan” <msnavjivan@gmail.com>
2024-07-03 02:11:20 -0400

    Added other fields of table to show fees data statically under fees management page

--- IMS-Frontend/src/assets/Components/AdminDashboard/FeesManagement/index.jsx
+++ IMS-Frontend/src/assets/Components/AdminDashboard/FeesManagement/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import * as Yup from "yup";
 import { Drawer } from "../../Common/Drawer";
+import { useFormik } from "formik";
 import ApiService from "../../../../Utils/ApiService";
 
 export const FeesManagement = () => {
@@ -12,10 +14,12 @@ export const FeesManagement = () => {
       setLoading(true);
       const resp = await ApiService.get(ApiService.ApiURLs.getCourses);
       if (resp.status === 200 && resp.data?.data) {
+        console.log(resp.data.data);
         setCourses(resp.data.data);
       }
       setLoading(false);
     } catch (err) {
+      console.log(err);
       setLoading(false);
     }
   };
@@ -26,7 +30,14 @@ export const FeesManagement = () => {
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Fees Collection</button>
+      <button
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Fees Collection
+      </button>
+
       <Drawer
         isOpen={isOpen}
         onClose={() => {
@@ -41,8 +52,9 @@ export const FeesManagement = () => {
         }
       >
         
-<div className="card-table-container">
-          <h2 className="form-heading">Harshad Satasiya - 11111111</h2>
+
+        <div className="card-table-container">
+          <h2 className="form-heading">Manthan Patel - 11111112</h2>
           <hr />
           <form>
             <div className="form-section">
@@ -76,6 +88,63 @@ export const FeesManagement = () => {
           </form>
         </div>
       </Drawer>
+    <br/>
+      <div className="form-container">
+          <div className="card">
+            <h2 className="form-heading">Fees Collections</h2>
+            <hr />
+            <div className="card-body">
+              <div className="form-group">
+                <label htmlFor="course">Select Student:</label>
+                <select id="course" name="course">
+                  <option value="" disabled selected>
+                    Choose Student
+                  </option>
+                  <option value="course1">Manthan Patel</option>
+                  <option value="course2">Harshad Satasiya</option>
+                  <option value="course3">John Doe</option>
+                  <option value="course4">Jane Joe</option>
+                </select>
+              </div>
+              <button className="btn" id="next-btn">Next</button>
+            </div>
+          </div>
+        </div>
+
+      <br/>
+        <h2 className="form-heading">Manthan Patel - 11111112</h2>
+        <hr />
+        <div className="card-table">
+          <table id="fees-table">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Installment Number</th>
+                <th>Payment Type</th>
+                <th>Invoice</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Dummy records */}
+              <tr>
+                <td>2024-06-15</td>
+                <td>500</td>
+                <td>1</td>
+                <td>Cash</td>
+                <td><button className="invoice-btn edit-btn">Invoice</button></td>
+              </tr>
+              <tr>
+                <td>2024-06-22</td>
+                <td>600</td>
+                <td>2</td>
+                <td>Card</td>
+                <td><button className="invoice-btn edit-btn">Invoice</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+     
     </>
   );
 };
