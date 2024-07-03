5c56511ec2746ab38f919a8af08af7e3b3d96ba4
“msnavjivan” <msnavjivan@gmail.com>
2024-07-03 01:45:45 -0400

    Added fetching courses for fees collection

--- IMS-Frontend/src/assets/Components/AdminDashboard/FeesManagement/index.jsx
+++ IMS-Frontend/src/assets/Components/AdminDashboard/FeesManagement/index.jsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Drawer } from "../../Common/Drawer";
+import ApiService from "../../../../Utils/ApiService";
 
 export const FeesManagement = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCourses = async () => {
+    try {
+      setLoading(true);
+      const resp = await ApiService.get(ApiService.ApiURLs.getCourses);
+      if (resp.status === 200 && resp.data?.data) {
+        setCourses(resp.data.data);
+      }
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCourses();
+  }, []);
 
   return (
     <>
