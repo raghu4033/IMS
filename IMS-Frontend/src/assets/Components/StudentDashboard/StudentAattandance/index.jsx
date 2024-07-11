eecec47d638edf8294c2f5ce428f5031c65d96f6
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-11 17:38:58 -0400

    Create student attandence view component and create route for that

---
+++ IMS-Frontend/src/assets/Components/StudentDashboard/StudentAattandance/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ApiService from "../../../../Utils/ApiService";
+import "../styles.css";
+
+export const StudentAattandance = () => {
+  return (
+    <>
+      <div className="card-table-container">
+        <h2 className="attendance-summary">Attendance Summary</h2>
+        <hr />
+        <div className="summary-cards">
+          <div className="attandence-card total-sessions">
+            <h3>Total Sessions</h3>
+            <p className="count">0</p>
+          </div>
+          <div className="attandence-card present">
+            <h3>Present</h3>
+            <p className="count">0</p>
+          </div>
+          <div className="attandence-card absent">
+            <h3>Absent</h3>
+            <p className="count">0</p>
+          </div>
+          <div className="attandence-card percentage">
+            <h3>Percentage</h3>
+            <p className="count">0%</p>
+          </div>
+        </div>
+      </div>
+      <div className="card-table-container">
+        <h2 className="form-heading">Attendance Details</h2>
+        <hr />
+        <div className="card-table">
+          <table id="attendance-table">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Absent/Present</th>
+                <th>Faculty</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2024-06-01</td>
+                <td>Present</td>
+                <td>Prof. B</td>
+              </tr>
+              <tr>
+                <td>2024-06-02</td>
+                <td>Absent</td>
+                <td>Prof. A</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
--- IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { StudentMenu } from "../../../Utils/Constants/StudentMenu";
 import { Layout } from "../Common/Layout";
 import { Dashboard } from "./Dashboard";
+import { StudentAattandance } from "./StudentAattandance";
 
 export const StudentDashboard = () => {
   return <Layout menu={StudentMenu}>
       <Routes>
         <Route path="dashboard/" element={<Dashboard />} />
+        <Route path="student-attandance/" element={<StudentAattandance />} />
       </Routes>
   </Layout>;
 };
