519caf7d57c71dc74043964bf2affd6889c12aa1
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-11 17:36:26 -0400

    Setup dashboard page and route for basic component

--- IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { StudentMenu } from "../../../Utils/Constants/StudentMenu";
 import { Layout } from "../Common/Layout";
+import { Dashboard } from "./Dashboard";
 
 export const StudentDashboard = () => {
-  return <Layout menu={StudentMenu}></Layout>;
+  return <Layout menu={StudentMenu}>
+      <Routes>
+        <Route path="dashboard/" element={<Dashboard />} />
+      </Routes>
+  </Layout>;
 };
