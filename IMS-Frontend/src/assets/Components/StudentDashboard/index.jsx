5bc3bc47f5e842defed82c42166b8b74ddfb43b1
“msnavjivan” <msnavjivan@gmail.com>
2024-07-18 01:57:02 -0400

    Added anouncement to student dashboard

--- IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/StudentDashboard/index.jsx
@@ -2,25 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import { StudentMenu } from "../../../Utils/Constants/StudentMenu";
 import { Layout } from "../Common/Layout";
 import { Dashboard } from "./Dashboard";
+import { StudentFees } from "./StudentFees";
 import { StudentProfile } from "./StudentProfile";
 import { StudentAattandance } from "./StudentAattandance";
-import { StudentClassSchedule } from "./StudentClassSchedule";
 import { StudentEvents } from "./StudentEvents";
-import { StudentAnnouncements } from "./StudentAnnouncements";
-import { StudentFees } from "./StudentFees";
+import { StudentNotice } from "./StudentNotice";
+import { StudentClasses } from "./StudentClasses";
 import { StudentGenerateCertificate } from "./StudentGenerateCertificate";
 
 export const StudentDashboard = () => {
-  return <Layout menu={StudentMenu}>
+  return (
+    <Layout menu={StudentMenu}>
       <Routes>
-        <Route path="dashboard/" element={<Dashboard />} />
         <Route path="student-profile/" element={<StudentProfile />} />
+        <Route path="dashboard/" element={<Dashboard />} />
         <Route path="student-attandance/" element={<StudentAattandance />} />
-        <Route path="student-class-schedule/" element={<StudentClassSchedule />} />
-        <Route path="student-events/" element={<StudentEvents />} />
-        <Route path="student-announcements/" element={<StudentAnnouncements />} />
         <Route path="student-fees/" element={<StudentFees />} />
+        <Route path="student-events/" element={<StudentEvents />} />
+        <Route path="student-announcements/" element={<StudentNotice />} />
+        <Route path="student-class-schedule/" element={<StudentClasses />} />
         <Route path="student-generate-certificate/" element={<StudentGenerateCertificate />} />
       </Routes>
-  </Layout>;
+    </Layout>
+  );
 };
