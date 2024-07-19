7296d8f45c1624bb52c4a4f701991c918050b46d
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-19 15:21:26 -0400

    Update loader with logo

---
+++ IMS-Frontend/src/assets/Components/Common/Loader/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoaderImg from "../../../Images/loaderimage.png";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <img src={LoaderImg} alt="Loading..." className="loader-image" />
+    </div>
+  );
+};
+
+export default Loader;
