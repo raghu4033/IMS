a79b7c11e115e07272a9db00728dec6e5bf177b5
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-19 14:55:28 -0400

    Add attandance and profile route in faculty dashboard

--- IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { FacultyMenu } from "../../../Utils/Constants/FacultyMenu";
 import { Layout } from "../Common/Layout";
 import { AnnouncementManagement } from "./AnnouncementManagement";
+import { AttendanceManagement } from "./AttendanceManagement";
+import { FacultyProfile } from "./FacultyProfile";
 
 export const FacultyDashboard = () => {
   return (
     <Layout menu={FacultyMenu}>
       <Routes>
         <Route path="/notice-board" element={<AnnouncementManagement />} />
+        <Route path="/attendances" element={<AttendanceManagement />} />
+        <Route path="/view-profile" element={<FacultyProfile />} />
       </Routes>
     </Layout>
   );
