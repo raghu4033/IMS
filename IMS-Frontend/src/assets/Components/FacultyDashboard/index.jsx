020a1337d6e533bd1cd2ac2ff0b9e80760050bb6
Harnaz14 <bajwaharnaz@gmail.com>
2024-07-19 00:34:25 -0400

    Modify faculty dashboard with some basic changes

--- IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
@@ -1,6 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import { FacultyMenu } from "../../../Utils/Constants/FacultyMenu";
 import { Layout } from "../Common/Layout";
+import { AnnouncementManagement } from "./AnnouncementManagement";
 
 export const FacultyDashboard = () => {
-  return <Layout menu={FacultyMenu}></Layout>;
+  return (
+    <Layout menu={FacultyMenu}>
+      <Routes>
+        <Route path="/notice-board" element={<AnnouncementManagement />} />
+      </Routes>
+    </Layout>
+  );
 };
