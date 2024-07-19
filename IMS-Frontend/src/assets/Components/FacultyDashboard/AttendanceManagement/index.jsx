d08c71ff6771e627c609d1f2f966af4457ce029f
raghu4033 <71260337+raghu4033@users.noreply.github.com>
2024-07-19 14:53:20 -0400

    Show attance record in faculty dashboard

---
+++ IMS-Frontend/src/assets/Components/FacultyDashboard/AttendanceManagement/index.jsx
@@ -0,0 +1,97 @@
+import moment from "moment";
+import { useEffect, useState } from "react";
+import ApiService from "../../../../Utils/ApiService";
+import { Table } from "../../Common/Table";
+import { AttendanceForm } from "./AttendanceForm";
+
+const columns = [
+  {
+    label: "Faculty Name",
+    key: "faculty",
+    renderValue: (value) => {
+      return [value?.firstName, value?.lastName].filter(Boolean).join(" ");
+    },
+  },
+  {
+    label: "Student Name",
+    key: "student",
+    renderValue: (value) => {
+      return [value?.firstName, value?.lastName].filter(Boolean).join(" ");
+    },
+  },
+  {
+    label: "Course Name",
+    key: "course",
+    renderValue: (value) => {
+      return value?.name || "N/A";
+    },
+  },
+  {
+    label: "Date",
+    key: "date",
+    renderValue: (value) => {
+      return value && moment(value).isValid()
+        ? moment(value).format("DD MMMM YYYY")
+        : "N/A";
+    },
+  },
+  {
+    label: "is Present?",
+    key: "isPresent",
+    renderValue: (value) => {
+      return value ? "Yes" : "No";
+    },
+  },
+];
+
+export const AttendanceManagement = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [attendances, setAttendances] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getAttendances = async () => {
+    try {
+      setLoading(true);
+      const resp = await ApiService.get(ApiService.ApiURLs.getAttendances);
+      if (resp.status === 200 && resp.data?.data) {
+        console.log(resp.data.data);
+        setAttendances(resp.data.data);
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAttendances();
+  }, []);
+
+  return (
+    <>
+      <div className="action-button">
+        <button
+          onClick={() => {
+            setIsOpen(true);
+          }}
+          disabled={loading}
+        >
+          Take Attendance
+        </button>
+      </div>
+
+      {!loading ? <Table rows={attendances} columns={columns} /> : <></>}
+
+      {isOpen ? (
+        <AttendanceForm
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          getAttendances={getAttendances}
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
