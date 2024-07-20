3e130b3054fabf1557689369041cd1cba54a4c85
“msnavjivan” <msnavjivan@gmail.com>
2024-07-20 02:03:19 -0400

    Modify and Created Faculty dashboard page

--- IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
+++ IMS-Frontend/src/assets/Components/FacultyDashboard/index.jsx
@@ -4,14 +4,22 @@ import { Layout } from "../Common/Layout";
 import { AnnouncementManagement } from "./AnnouncementManagement";
 import { AttendanceManagement } from "./AttendanceManagement";
 import { FacultyProfile } from "./FacultyProfile";
+import { Dashboard } from "../FacultyDashboard/Dashboard";
+import { StudentInformation } from "../FacultyDashboard/StudentInformation";
+import { Submissions } from "../FacultyDashboard/Submissions";
+import { StudentInquiry } from "../FacultyDashboard/StudentInquiry";
 
 export const FacultyDashboard = () => {
   return (
     <Layout menu={FacultyMenu}>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/notice-board" element={<AnnouncementManagement />} />
         <Route path="/attendances" element={<AttendanceManagement />} />
+        <Route path="/student-information" element={<StudentInformation />} />
         <Route path="/view-profile" element={<FacultyProfile />} />
+        <Route path="/submissions" element={<Submissions />} />
+        <Route path="/student-inquiry" element={<StudentInquiry />} />
       </Routes>
     </Layout>
   );
