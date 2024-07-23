2d70ecc741f6e1ceebbbdf84e423258f7de1419a
“msnavjivan” <msnavjivan@gmail.com>
2024-07-23 16:38:49 -0400

    Modify the user api and services with minor changes and package file as well

--- IMS-Backend/server/services/user.service.js
+++ IMS-Backend/server/services/user.service.js
@@ -4,13 +4,17 @@ const { BadRequestError } = require("../utils/error");
 const moment = require("moment/moment");
 const { Constants } = require("../utils/constants");
 
-const { User } = mongoose.models;
+const { User, Fees, Event, 'Student-Inquiry': StudentInquiry, ClassSchedule } = mongoose.models;
 
 async function getUsers({ query }) {
   try {
     const { role, course } = query;
+    const filters = {};
+
+    if (role) filters.role = role;
+    if (course) filters.course = course;
     const users = await User.find(
-      { role, course },
+      filters,
       { password: 0 },
       { sort: { updatedAt: -1 } }
     ).populate("course");
@@ -201,9 +205,60 @@ async function createFacultyUser(body) {
   }
 }
 
+async function getDashboardSummary(reqUser) {
+  try {
+
+    if (reqUser?.role !== Constants.Role.ADMIN) {
+      throw new BadRequestError("You are not authorised to access this resource.");
+    }
+
+    const totalStudents = await User.countDocuments({
+      role: Constants.Role.STUDENT
+    })
+
+    const totalFaculties = await User.countDocuments({
+      role: Constants.Role.FACULTY
+    })
+
+    const totalEvents = await Event.countDocuments({})
+
+    const totalFees = await Fees.aggregate().group({
+      _id: null,
+      total: {
+        $sum: "$feesAmount"
+      }
+    });
+
+    const latest2Inquiry = await StudentInquiry.find({}, {
+      name: 1, createdAt: 1, joiningDate: 1, fullName: 1
+    }, { sort: { createdAt: -1 }, limit: 2 })
+
+    const latest2Event = await Event.find({}, {
+      name: 1, date: 1
+    }, { sort: { createdAt: -1 }, limit: 2 })
+
+    const latest2ClassSchedule = await ClassSchedule.find({}, {
+      fromDate: 1, course: 1
+    }, { sort: { createdAt: -1 }, limit: 2 }).populate("course", "name")
+
+    return {
+      totalStudents,
+      totalFaculties,
+      totalFees: totalFees?.[0]?.total ?? 0,
+      totalEvents,
+      latest2Inquiry,
+      latest2Event,
+      latest2ClassSchedule
+    }
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
 module.exports = {
   getUsers,
   createAdminUser,
   createStudentUser,
-  createFacultyUser
+  createFacultyUser,
+  getDashboardSummary
 };
