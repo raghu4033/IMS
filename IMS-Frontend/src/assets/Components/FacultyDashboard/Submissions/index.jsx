ce67a5a0c64aa28fa46f72b0b63ef20bdb7c7ff0
Harnaz14 <bajwaharnaz@gmail.com>
2024-08-14 19:58:36 -0400

    Remove old component

--- IMS-Frontend/src/assets/Components/FacultyDashboard/Submissions/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Submissions = () => {
-  return (
-    <div className="simple-component">
-      <h1 className="heading">Simple Heading</h1>
-    </div>
-  );
-};
